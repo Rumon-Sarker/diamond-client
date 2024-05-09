d46e901f8abd25a824599c8bf36a99e824f12a88
Rumon-Sarker <mdsrrumon1234@gmail.com>
2024-05-09 17:55:39 +0600

    not fount page to home page link

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,13 +1,42 @@
 import { Helmet } from "react-helmet-async";
 
 const Blogs = () => {
+    const data = [];
     return (
         <div>
             <Helmet>
                 <title>Diamond || Blog</title>
             </Helmet>
             <div>
-                <h1>Comming Sonnnnn...............</h1>
+                <div className="text-center mt-10">
+                    <h1 className="text-3xl font-bold">Our <span className="text-orange-500">Blog</span></h1>
+                    <p className="my-5">Praesentium rem sequi voluptatum velit tempore dicta
+                        <br />dolor sit amet ex eum consectetur,debitis quibusdam ea rem vitae rem?.</p>
+                </div>
+
+                <div className=" grid  grid-cols-2 md:grid-cols-4 sm:grid-cols-3 gap-7 mt-20">
+
+                    {
+                        data?.map((items, index) => <div key={index}>
+                            <div className="card bg-base-100 shadow-xl">
+                                <img src={items?.imgCdn} alt="" />
+                                <div className="card-body ">
+                                    <h2 className="card-title">{items?.title}</h2>
+                                    <p>{items?.desc}</p>
+
+                                </div>
+                                <div className='my-2 ml-7'>
+                                    <Link href={`/blog-details/${items?.id}`} className='btn btn-success'>
+                                        Blog Details
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>)
+                    }
+
+
+                </div>
+
             </div>
         </div>
     );
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 import notFoundImg from "../../assets/BannerImg/not-found-3.jpg";
+import { IoIosArrowRoundBack } from "react-icons/io";
 const NotFound = () => {
     return (
         <div>
-            <img className=" mx-auto w-6/12" src={notFoundImg} alt="not-found" />
+            <div className="italic md:mx-auto bg-slate-50">
+                <Link className=" items-center w-24 text-black" to={"/"}>
+                    <h1 className="text-xl flex items-center gap-1"><span className="text-4xl font-bold "><IoIosArrowRoundBack /></span> Go to home</h1>
+                </Link>
+            </div>
+            <img className=" mx-auto w-screen  md:w-6/12" src={notFoundImg} alt="not-found" />
         </div>
     );
 };
